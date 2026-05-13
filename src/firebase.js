6e1023b24a3fb5eb11b917237a3d5f9bd9989a8d
CPN.CDT <naluzchristian16@gmail.com>
2026-05-13 22:53:07 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCSSId4EnYdoeoex_-1zLl327kbyWgbbds",
+  authDomain: "cdtpos-2946a.firebaseapp.com",
+  projectId: "cdtpos-2946a",
+  storageBucket: "cdtpos-2946a.firebasestorage.app",
+  messagingSenderId: "952364279531",
+  appId: "1:952364279531:web:8630a2203c15a7d10340ae",
+  measurementId: "G-0RSY50BRFJ"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
